0289463126f76331aba5aafe144adb37ad0c86ef
Dmitry Khrunin <thesunshineofficial@gmail.com>
2023-11-10 19:53:32 +0300

    Fix migration

--- EmployeeWebApp.Infrastructure/Migrations/Script0001 - Create tables.sql
+++ EmployeeWebApp.Infrastructure/Migrations/Script0001 - Create tables.sql
@@ -1,32 +1,32 @@
 CREATE TABLE IF NOT EXISTS public."Companies"
 (
     "Id" integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "Name" character varying(50)[] NOT NULL,
+    "Name" character varying(50) NOT NULL,
     PRIMARY KEY ("Id")
 );
 
 CREATE TABLE IF NOT EXISTS public."Passports"
 (
     "Id" integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "Type" character varying(50)[] NOT NULL,
-    "Number" character varying(50)[] NOT NULL,
+    "Type" character varying(50) NOT NULL,
+    "Number" character varying(50) NOT NULL,
     PRIMARY KEY ("Id")
 );
 
 CREATE TABLE IF NOT EXISTS public."Departments"
 (
     "Id" integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "Name" character varying(50)[] NOT NULL,
-    "Phone" character varying(20)[] NOT NULL,
+    "Name" character varying(50) NOT NULL,
+    "Phone" character varying(20) NOT NULL,
     PRIMARY KEY ("Id")
 );
 
 CREATE TABLE IF NOT EXISTS public."Employees"
 (
     "Id" integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    "Name" character varying(50)[] NOT NULL,
-    "Surname" character varying(50)[] NOT NULL,
-    "Phone" character varying(20)[] NOT NULL,
+    "Name" character varying(50) NOT NULL,
+    "Surname" character varying(50) NOT NULL,
+    "Phone" character varying(20) NOT NULL,
     "CompanyId" integer NOT NULL references "Companies"("Id"),
     "PassportId" integer NOT NULL references "Passports"("Id"),
     "DepartmentId" integer NOT NULL references "Departments"("Id"),
